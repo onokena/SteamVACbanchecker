49dc967c87a77b61ea708c5d64babbcf718513b8
onokena <luiliev00@gmail.com>
2024-02-09 13:12:56 +0200

    Preparation for Deployment

--- server.js
+++ server.js
@@ -6,26 +6,20 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-// Middleware to parse JSON request bodies
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-// Route to handle Steam API requests
 app.get('/api/steam/:steamId', async (req, res) => {
     try {
         const apiKey = process.env.STEAM_API_KEY;
         const steamId = req.params.steamId;
 
-        // Fetch account creation date
         const accountCreationDate = await getAccountCreationDate(steamId, apiKey);
         
-        // Check VAC ban status
         const vacBanStatus = await checkVacBanStatus(steamId, apiKey);
         
-        // Make request to Steam API to fetch data
         const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${apiKey}&steamids=${steamId}`);
 
-        // Send data back to the client
         res.json({ accountCreationDate, vacBanStatus });
     } catch (error) {
         console.error('Error:', error);
